d980fa246b5bfbffbb231922fcb78939b4aa4f09
Matthew (Qilong) Chen <qchen1@haverford.edu>
2019-12-14 21:23:55 -0500

    Create post route to /api/todos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    todoRouters = require('./routes/todos');
+    todoRouters = require('./routes/todos'),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.send("Hello from the root");
